6db78478776036b2e501492b048655581cb9f0bc
Michal Moudrý <moudry.michal@live.com>
2025-04-01 19:36:43 +0200

    new restart

--- src/lib/components/atoms/Link.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    /** @type {{ url: string, title: string, content: string, classes: string }} */
-    let { url, title, content, classes = '' } = $props();
-</script>
-
-<a href="https://www.{url}" {title} class="text-blue-800 {classes} hover:underline">{content}</a>
--- src/lib/components/atoms/Pill.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  /** @type {{ text: string }} */
-  let { text } = $props();
-</script>
-
-<span class="bg-primary-300 text-black rounded px-2">{text}</span>
