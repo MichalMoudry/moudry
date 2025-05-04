42720dd9cb2bd1f0a7daa43ebd3bc39e755d46d1
Michal Moudrý <moudry.michal@live.com>
2025-05-04 19:22:49 +0200

    Switched to plan HTML+CSS (#13)

--- src/components/atoms/Icon.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-const props = defineProps({
-  icon: String
-})
-</script>
-
-<template>
-  <span class="oi" :data-glyph="props.icon" />
-</template>
--- src/components/atoms/Pill.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-defineProps({
-    text: String
-})
-</script>
-
-<template>
-    <span class="bg-primary-300 text-black rounded px-2 py-1">{{ text }}</span>
-</template>
