d55c6f16dc4cfc5cff9697929519446d50e6a08a
Michal Moudrý <moudry.michal@live.com>
2025-05-11 20:43:00 +0200

    build pipeline fix
    fix + optimizations

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function (environment) {
   const ENV = {
-    modulePrefix: 'ember-quickstart',
+    modulePrefix: 'moudry',
     environment,
     rootURL: '/',
     locationType: 'history',
