37b9d0747849976292ed17fad28401eea724d963
Michal Moudrý <moudry.michal@live.com>
2025-05-17 12:02:55 +0200

    Revert "build pipeline fix"
    This reverts commit d55c6f16dc4cfc5cff9697929519446d50e6a08a.

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function (environment) {
   const ENV = {
-    modulePrefix: 'moudry',
+    modulePrefix: 'ember-quickstart',
     environment,
     rootURL: '/',
     locationType: 'history',
